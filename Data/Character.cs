e2d1a2fb7d049592adc2b10b2793e5098bd2639c
Raiser00 <damien.khashan@ifosup.wavre.be>
2025-01-05 11:28:44 +0100

    Config parametre Class Character name lvl hp

---
+++ Data/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace MasterDuell.Data;
+
+public abstract class Character
+{
+    public string Name { get; set; }
+    //Parametre de gestion de niveau
+    private int level = 1;
+    public int Level
+    {
+        get { return level; }
+        set
+        {
+            if (level! > value)
+            {
+                level = value;
+            }
+        }
+    }
+    public int CurrentXp { get; set; } = 0;
+    public int XpToNextLevel { get; set; } = 100;
+    //Parametre de gestion de vie
+    public abstract int MaxHitPoints { get; set; }
+    public abstract int CurrentHitPoints { get; set; }
+}
+
